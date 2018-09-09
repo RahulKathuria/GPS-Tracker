7d90ffe98a383a3cce64e71504de583c3936de6e
RahulKathuria <rahulkathuria52@gmail.com>
2018-09-09 23:33:38 +0530

    Added Intents

--- app/src/main/java/com/cafedroid/gpstrackerapp/NameActivity.java
+++ app/src/main/java/com/cafedroid/gpstrackerapp/NameActivity.java
@@ -1,13 +1,28 @@
 package com.cafedroid.gpstrackerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NameActivity extends AppCompatActivity {
+
+    String email,password;
+    EditText e5;
+    Button b6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+        e5 = findViewById(R.id.editText5);
+        b6 = findViewById(R.id.button6);
+
+        Intent myIntent = getIntent();
+        if(myIntent!=null){
+            email = myIntent.getStringExtra("email");
+            password = myIntent.getStringExtra("password");
+        }
     }
 }
