0bba2d98e5da502ca7bb7641df78f7d0f5769f49
RahulKathuria <rahulkathuria52@gmail.com>
2018-10-09 02:31:07 +0530

    Completed Basic Strucutre

--- app/src/main/java/com/cafedroid/gpstrackerapp/MainActivity.java
+++ app/src/main/java/com/cafedroid/gpstrackerapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cafedroid.gpstrackerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToLogin(View v){
+
+        Intent myIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(myIntent);
+    }
+
+
+    public void goToRegister(View v){
+
+        Intent myIntent = new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(myIntent);
     }
 }
