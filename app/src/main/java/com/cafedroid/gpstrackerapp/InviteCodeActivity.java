f5e01ecd1fa7be8988b169e3b31aa28f9cca1a3a
RahulKathuria <rahulkathuria52@gmail.com>
2018-10-12 05:16:56 +0530

    Added Create user class

--- app/src/main/java/com/cafedroid/gpstrackerapp/InviteCodeActivity.java
+++ app/src/main/java/com/cafedroid/gpstrackerapp/InviteCodeActivity.java
@@ -3,6 +3,7 @@ package com.cafedroid.gpstrackerapp;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.CircularProgressDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,9 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -51,6 +55,12 @@ public class InviteCodeActivity extends AppCompatActivity {
     public void registerUser(View v){
         dialog.setMessage("Wait while your account is being created");
         dialog.show();
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+            }
+        });
     }
 
 }
